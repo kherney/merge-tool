18e4b6907dd3051e1e30da4d57e70054ba158561
Kevin <kevinh939@gmail.com>
2023-01-30 09:56:10 -0500

    [FIX] Fix different way to write excel buffer

--- merge.py
+++ merge.py
@@ -1,7 +1,5 @@
 import pandas as pd
-from pandas.io.excel import ExcelFile
-from pandas.core.indexes.base import Index
-from pandas import isna
+from pandas.io.excel import ExcelFile, ExcelWriter
 
 from argparse import ArgumentParser
 
@@ -22,16 +20,20 @@ def update_data(data: Dict, cell: Any, key: Any, n_cell: int, delimiter: str = '
 
 def main():
     data = {}
+    file_name = args.out_file
+
+    if not file_name:
+        file_name = "File Merged"
+
+    file = ExcelFile(args.in_file, engine='openpyxl')
 
     def create_row():
         if key not in data.keys():
             data.update({key: ['' for _ in range(len(row))]})
 
     n_column = args.merge_by
-    file = ExcelFile(args.in_file, engine='openpyxl')
     dt = pd.read_excel(file)
     labels = dt.columns.values
-    data.update({'label': labels})
 
     for index, series in dt.iterrows():
         row = series.values
@@ -44,10 +46,8 @@ def main():
 
     df = pd.DataFrame(data=data.values())
 
-    file_name = args.out_file
-    if not file_name:
-        file_name = "File Merged"
-    df.to_excel("{}.xlsx".format(file_name), sheet_name='Sheet_name_1')
+    with ExcelWriter("{}.xlsx".format(file_name), engine='openpyxl') as out_file:
+        df.to_excel(out_file, sheet_name=args.in_file, index=False, header=labels)
 
 
 if __name__ == '__main__':
@@ -55,9 +55,9 @@ if __name__ == '__main__':
     parser = ArgumentParser(prog="Merge Tool",
                             conflict_handler='resolve',
                             description=" Script for merge rows by column name")
-    parser.add_argument("--merge_by", type=int, dest="merge_by", required=True,
+    parser.add_argument('-m', "--merge_by", type=int, dest="merge_by", required=True,
                         help="N: Positional Column in the given file. From zero 0 to N-1")
-    parser.add_argument("--file", type=str, dest="in_file", required=True,
+    parser.add_argument('-f', "--file", type=str, dest="in_file", required=True,
                         help="File to load")
     parser.add_argument("-o", "--out_path", type=str, dest="out_file", required=False,
                         help="File to output")
